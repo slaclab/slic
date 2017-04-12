881ecd3537ebcd34ffd1cf839169e6f8768d1dba
JeremyMcCormick <jermccormick@gmail.com>
2017-04-12 16:04:44 -0700

    update GDML cmake config

--- cmake/FindGDML.cmake
+++ cmake/FindGDML.cmake
@@ -1,18 +1,10 @@
-FIND_PATH( GDML_DIR G4Binding/G4Processor/G4Processor/GDMLProcessor.h )
+FIND_PATH( GDML_DIR include/G4Processor/GDMLProcessor.h )
 
 FIND_LIBRARY( GDML_LIBRARY gdml ${GDML_DIR}/lib/ )
 
-LIST( APPEND GDML_INCLUDE_DIRS 
-  ${GDML_DIR}/Common/Processes
-  ${GDML_DIR}/Common/Saxana  
-  ${GDML_DIR}/Common/Schema  
-  ${GDML_DIR}/Common/Writer
-  ${GDML_DIR}/G4Binding/G4Evaluator
-  ${GDML_DIR}/G4Binding/G4Processor
-  ${GDML_DIR}/G4Binding/G4Subscribers
-  ${GDML_DIR}/G4Binding/G4Writer
-  ${GDML_DIR}/STEPBinding/STEPWriter
-)
+SET( GDML_LIBRARY ${GDML_LIBRARY} )
+
+SET( GDML_INCLUDE_DIR ${GDML_DIR}/include )
 
 INCLUDE( FindPackageHandleStandardArgs )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( GDML DEFAULT_MSG GDML_DIR )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( GDML DEFAULT_MSG GDML_LIBRARY GDML_DIR )
