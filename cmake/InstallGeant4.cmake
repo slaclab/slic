b0d35003d473951eb59389726b81c166eb8aec39
Jeremy McCormick <jermccormick@gmail.com>
2017-04-19 16:57:42 -0700

    CMake build enhancements and bug fixes (#23)
    * change GLOBAL_INSTALL_DIR to CMAKE_INSTALL_PREFIX
    
    * use unquoted version variables in cmake install scripts
    
    * comment out debug prints
    
    * correct slic bin dir
    
    * install ext dep sources directly in build dir

--- cmake/InstallGeant4.cmake
+++ cmake/InstallGeant4.cmake
@@ -3,23 +3,28 @@ IF( NOT Geant4_VERSION )
 ENDIF()
 
 IF ( Geant4_VERSION EQUAL "master" )
-    MESSAGE( FATAL "Installing Geant4 from github master is not allowed." )
+    MESSAGE( FATAL_ERROR "Installing Geant4 from github master is not allowed." )
+ENDIF()
+
+IF ( NOT GEANT4_INSTALL_DATA )
+    SET( GEANT4_INSTALL_DATA "OFF" )
 ENDIF()
 
 EXTERNALPROJECT_ADD(
     Geant4
 
     GIT_REPOSITORY "https://github.com/Geant4/geant4"
-    GIT_TAG "${GEANT4_VERSION}"
+    GIT_TAG ${GEANT4_VERSION}
     
     UPDATE_COMMAND ""
     PATCH_COMMAND ""
 
-    SOURCE_DIR "${CMAKE_BINARY_DIR}/extdep/geant4"
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${GLOBAL_INSTALL_DIR}/geant4 -DGEANT4_INSTALL_DATA=ON
+    SOURCE_DIR "${CMAKE_BINARY_DIR}/geant4"
+    
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}/geant4 -DGEANT4_INSTALL_DATA=${GEANT4_INSTALL_DATA}
     
     BUILD_COMMAND make -j4
 )
 
 STRING( REPLACE "v" "" Geant4_VERSION_NUMBER ${Geant4_VERSION} )
-SET( Geant4_DIR ${GLOBAL_INSTALL_DIR}/geant4/lib64/Geant4-${Geant4_VERSION_NUMBER} CACHE PATH "Geant4 install dir" FORCE )
+SET( Geant4_DIR ${CMAKE_INSTALL_PREFIX}/geant4/lib64/Geant4-${Geant4_VERSION_NUMBER} CACHE PATH "Geant4 install dir" FORCE )
