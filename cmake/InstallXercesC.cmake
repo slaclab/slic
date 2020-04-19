5e9de777dfe0a09c3e17b2dc8567f782ebbff866
Jeremy McCormick <jermccormick@gmail.com>
2020-04-19 01:21:17 -0500

    Add var for XercesC base dir.

--- cmake/InstallXercesC.cmake
+++ cmake/InstallXercesC.cmake
@@ -11,5 +11,6 @@ EXTERNALPROJECT_ADD(
     CONFIGURE_COMMAND "${CMAKE_BINARY_DIR}/xerces/configure" --enable-shared --disable-static --prefix=${DEPENDENCY_INSTALL_DIR}/xerces
 )
 
+set( XercesC_DIR ${DEPENDENCY_INSTALL_DIR}/xerces CACHE PATH "XercesC install dir" FORCE )
 SET( XercesC_INCLUDE_DIR ${DEPENDENCY_INSTALL_DIR}/xerces/include CACHE PATH "XercesC include dir" FORCE )
 SET( XercesC_LIBRARY ${DEPENDENCY_INSTALL_DIR}/xerces/lib/libxerces-c.so CACHE PATH "XercesC library" FORCE )
