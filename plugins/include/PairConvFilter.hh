df7b8a859a3a35ed3b90c0f261e572eeb245dadb
Omar Moreno <email@omarmoreno.net>
2022-01-03 16:37:16 -0800

    Add a stacking action to force processing of the photon first. Include a summary of all conversions found at the end of the run.

--- plugins/include/PairConvFilter.hh
+++ plugins/include/PairConvFilter.hh
@@ -26,10 +26,16 @@ public:
   std::string getName() { return "PairConvFilter"; }
 
   /// @return True if this plugin has a stepping action
-  bool hasSteppingAction() { return true; }
+  bool hasSteppingAction() final override { return true; }
 
   /// @return True if this plugin has an event action
-  bool hasEventAction() { return true; }
+  bool hasEventAction() final override { return true; }
+
+  /// @return True if this plugin has a stacking action
+  bool hasStackingAction() final override { return true; }
+
+  /// @return True if this plugin has a run action
+  bool hasRunAction() final override { return true; } 
 
   /**
    * Implementmthe stepping action which performs the filtering.
@@ -45,9 +51,35 @@ public:
    */
   void endEvent(const G4Event *) final override;
 
+  /**
+   * Method called at the end of the run.  This will be used to print out
+   * a summary of the total conversion statistics.
+   *
+   * @param run Geant4 run object.
+   */
+  void endRun(const G4Run* run) final override; 
+
+  /**
+   * Classify a track. In the case of this filter, the track is pushed to the
+   * waiting stack if a pair conversion hasn't been found and the particle
+   * is not a photon.
+   *
+   * @param currentTrackClass The current track classification.
+   * @return A new track classification if the particle is not a photon.
+   */
+  G4ClassificationOfNewTrack stackingClassifyNewTrack(
+      const G4Track *,
+      const G4ClassificationOfNewTrack &currentTrackClass) final override;
+
 private:
+  /// Pointer to the current track being processed. 
+  G4Track *currentTrack_{nullptr};
+  
   /// Flag that denotes whether a conversion has been found.
   bool hasPairConv_{false};
+
+  /// Counter to keep track of the pair conversion count
+  short pairConvCount_{0};
 
 }; // PairConvFilter
 } // namespace slic
